25f13f3ba5d47fc84bd7d485201cecbb3b6fcb54
rohitqbay <147059736+rohitqbay@users.noreply.github.com>
2023-10-11 16:02:35 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+
 document.addEventListener('DOMContentLoaded', function () {
     loadProduct();
 });
